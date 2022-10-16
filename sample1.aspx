06e60583b96528d0efc0ce289a881537ec86d8b4
suzuki <evo0622@gmail.com>
2022-10-17 00:34:32 +0900

    create insert

--- sample1.aspx
+++ sample1.aspx
@@ -3,14 +3,43 @@
 <!DOCTYPE html>
 
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head runat="server">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <title></title>
+<head>
+    <title>aspを移植</title>
+    <script type="text/javascript">
+        function touroku() {
+            //クライアント側の入力チェック
+            if (document.frmMain.txtName.value == '') {
+                alert('商品名を入力してください。');
+                return false;
+            }
+            if (document.frmMain.txtPrice.value == '') {
+                alert('単価を入力してください。');
+                return false;
+            }
+            return true;
+        }
+        function init() {
+            if (document.frmMain.hidCheck.value == "1") {
+                alert("入力した商品名はすでに登録されています。");
+            }
+        }
+    </script>
 </head>
-<body>
-    <form id="form1" runat="server">
-        <div>
-        </div>
+<body onload="init()">
+    <form id="frmMain"  method="post"  runat="server">
+        <!-- buttonをsubmitコントロールに変更します -->
+        <input id="btnTouroku" type="submit" value="登録" onclick="return touroku()" runat="server">
+        商品名：<input type="text" name="txtName" id="txtName" runat="server">
+        単価：<input type="text" name="txtPrice" id="txtPrice" runat="server">
+        <asp:GridView ID="grvDetail" runat="server" AutoGenerateColumns="false">
+            <Columns>
+                <asp:BoundField DataField="NAME" HeaderText="商品名" />
+            </Columns>
+            <Columns>
+                <asp:BoundField DataField="PRICE" HeaderText="単価" />
+            </Columns>
+        </asp:GridView>
+        <input type="hidden" name="hidCheck" id="hidCheck" runat="server" />
     </form>
 </body>
 </html>
--- sample1.aspx.vb
+++ sample1.aspx.vb
@@ -1,8 +1,116 @@
-﻿Public Class sample1
+﻿Imports Oracle.ManagedDataAccess.Client
+
+Public Class sample1
     Inherits System.Web.UI.Page
 
+    Public detail As String = String.Empty
+    Public hidCheck As String = String.Empty
+    Public strName As String = String.Empty
+    Public strPrice As String = String.Empty
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-
+        '初期表示時のみこのタイミングで処理を行います
+        If Not IsPostBack Then
+            Dim dt As DataTable = GetData() 'データ取得
+            DispData(dt) 'データ表示
+        End If
     End Sub
+
+
+    ''' <summary>
+    ''' データ取得
+    ''' </summary>
+    Private Function GetData() As DataTable 'Page_Loadで実行
+        'データテーブルを宣言
+        Dim dt As DataTable = New DataTable()
+
+        Dim Sql As String = "SELECT name,price FROM SYOUHIN"
+        Try
+            Using Conn As OracleConnection = New OracleConnection()
+                Conn.ConnectionString =
+                     "User Id=system;Password=Oracle18c;Data Source=localhost/XE;"
+                Conn.Open()
+                Using cmd As OracleCommand = New OracleCommand(Sql)
+                    cmd.Connection = Conn
+                    cmd.CommandType = CommandType.Text
+                    Using reader As OracleDataReader = cmd.ExecuteReader()
+                        'テーブルデータに代入
+                        dt.Load(reader)
+                    End Using
+                End Using
+            End Using
+            Return dt
+        Catch ex As Exception
+            Console.WriteLine(ex.Message)
+        End Try
+    End Function
+
+    ''' <summary>
+    ''' データ表示
+    ''' </summary>
+    Private Sub DispData(ByVal dt As DataTable)
+        grvDetail.DataSource = dt
+        grvDetail.DataBind()
+    End Sub
+
+    ''' <summary>
+    ''' 登録データをチェック
+    ''' </summary>
+    Private Function CheckData() As Boolean
+        'データが存在するかどうかチェックする処理は省略します
+        'すでにデータが登録されていたらtrueを返します
+    End Function
+
+    ''' <summary>
+    ''' データ登録
+    ''' </summary>
+    Private Sub InsertData()
+        Dim Sql As String = "insert into syouhin (name,price) VALUES (:name,:price)"
+        Dim Name As String
+        Name = Request.Form("txtName")
+        Dim Price As String
+        Price = Request.Form("txtPrice")
+
+        Using Conn As OracleConnection = New OracleConnection()
+
+            Conn.ConnectionString =
+          "User Id=system;Password=Oracle18c;Data Source=localhost/XE;"
+            Try
+                Conn.Open()
+
+                Using transaction As OracleTransaction = Conn.BeginTransaction()
+
+                    Try
+                        Using cmd As OracleCommand = New OracleCommand(Sql)
+
+                            cmd.Connection = Conn
+                            cmd.CommandType = CommandType.Text
+                            cmd.BindByName = True
+
+                            cmd.Parameters.Add(New OracleParameter("name", OracleDbType.Varchar2, Name, ParameterDirection.Input))
+                            cmd.Parameters.Add(New OracleParameter("price", OracleDbType.Varchar2, Price, ParameterDirection.Input))
+                            'cmd.Parameters.Add(New OracleParameter("price", OracleDbType.Varchar2)).Value = "2000"
+
+                            cmd.ExecuteNonQuery()
+                            transaction.Commit()
+                        End Using
+                    Catch ex As Exception
+                        transaction.Rollback()
+                        Console.WriteLine(ex.Message)
+                    End Try
+                End Using
+            Catch ex As Exception
+                Console.WriteLine(ex.Message)
+            End Try
+        End Using
+    End Sub
+
+    Private Sub btnTouroku_ServerClick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnTouroku.ServerClick
+
+        InsertData()
+
+        Dim dt As DataTable = GetData() 'データ取得
+        DispData(dt) 'データ表示
+    End Sub
+
 
 End Class
